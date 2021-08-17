a47c0bb4b9e3f144a0126e63f3e59c281bb05174
piyx <piyx.abhishek@gmail.com>
2021-08-17 17:40:05 +0530

    Added log and subscribe to channels func

--- migrate.py
+++ migrate.py
@@ -2,23 +2,45 @@ import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
-scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+import json
+from dataclasses import dataclass
+
+
 API_SERVICE_NAME = "youtube"
 API_VERSION = "v3"
 CLIENT_SECRETS_FILE = "clientsecret.json"
 
-# Get credentials and create an API client
-flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes)
-credentials = flow.run_console()
-youtube = googleapiclient.discovery.build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
 
-def get_subscription_ids(youtube):
-    subscription_ids = []
+@dataclass
+class YoutubeChannel:
+    name: str
+    id: str
+    desc: str
+    thumbnail: str
+
+
+def create_api_client(scopes: list[str]) -> googleapiclient.discovery.Resource:
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes)
+    credentials = flow.run_console()
+    youtube = googleapiclient.discovery.build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+    return youtube
+
+
+def get_subscribed_channels() -> list[YoutubeChannel]:
+    '''
+    Fetch all your subscribed channels.
+    Choose your old google account (Which has all subscribed channels) when redirected.
+    '''
+    scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+    youtube = create_api_client(scopes)
+
+
+    subscribed_channels = []
     fetch = True
     page_token = None
     while fetch:
         request = youtube.subscriptions().list(
-            part="id",
+            part="id,snippet",
             channelId="UC3w7CEjnByJQcOdsltrqqdA",
             pageToken=page_token
         )
@@ -27,8 +49,74 @@ def get_subscription_ids(youtube):
         fetch = page_token is not None
 
         for item in response['items']:
-            subscription_ids.append(item['id'])
+            snippet = item['snippet']
+            youtube_channel = YoutubeChannel(
+                name=snippet['title'],
+                id=snippet['resourceId']['channelId'],
+                desc=snippet['description'],
+                thumbnail=snippet['thumbnails']['high']['url']
+            )
+            subscribed_channels.append(youtube_channel)
     
-    return subscription_ids
+    return subscribed_channels
 
-print(len(get_subscription_ids(youtube)))
+
+def write_subscribed_channels(subscribed_channels: list[YoutubeChannel]) -> None:
+    '''
+    Write all your subscribed channels data into a file.
+    Use this function in case you want to keep a log, else
+    this function can be ignored
+    '''
+    with open("subscribed_channels.json", "w") as f:
+        json.dump([youtube_channel.__dict__ for youtube_channel in subscribed_channels], f)
+
+
+def subscribe_channels(youtube_channels: list[YoutubeChannel]) -> None:
+    '''
+    Subscribes to all the channels from the youtube_channels list.
+    Choose your new google account where you want to add
+    these channels to your subscription list.
+    '''
+    scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
+    youtube = create_api_client(scopes)
+
+    total_channels = len(youtube_channels)
+    for i, youtube_channel in enumerate(youtube_channels, 1):
+        request = youtube.subscriptions().insert(
+            part="id,snippet",
+            body={
+            "snippet": {
+                    "resourceId": {
+                    "channelId": youtube_channel.id,
+                    "kind": "youtube#subscription"
+                    }
+                }
+            }
+        )
+
+        print(f"Subscribing to {youtube_channel.name} [{i}/{total_channels}]")
+        try:
+            response = request.execute()
+        except Exception as e:
+            print(e)
+
+
+def main():
+    print("--------------------------------------------------------------------------------------------")
+    print("Click the link and choose the google account to fetch the youtube subscribed channels from: ")
+    subscribed_channels = get_subscribed_channels()
+
+
+    # Uncomment the code below if you want to store the list of channels in a json file
+    # write_subscribed_channels(subscribed_channels)
+
+    print("-----------------------------------------------------------------------------------------------------")
+    print("Click the link and choose the google account to which you want to add youtube channel subscriptions: ")
+    subscribe_channels(subscribed_channels)
+
+    print("------------------------------------------------------------------------------------------")
+    print("Successfully migrated all your youtube subscribed channels from old account to new account")
+
+
+if __name__=='__main__':
+    main()
